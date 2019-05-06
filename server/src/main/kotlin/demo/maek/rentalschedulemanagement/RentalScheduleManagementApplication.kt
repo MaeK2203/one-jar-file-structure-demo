bf8156b646691cd92cc80673e83f6b405999769b
Maek2203 <g.guzas.r@gmail.com>
2019-05-06 19:27:29 +0900

    Base creation.

---
+++ server/src/main/kotlin/demo/maek/rentalschedulemanagement/RentalScheduleManagementApplication.kt
@@ -0,0 +1,11 @@
+package demo.maek.rentalschedulemanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RentalScheduleManagementApplication
+
+fun main(args: Array<String>) {
+    runApplication<RentalScheduleManagementApplication>(*args)
+}
